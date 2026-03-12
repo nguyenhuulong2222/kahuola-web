759b9b3054225f476ead377c221fda530ce5717a
nguyenhuulong2222 <3ranknguyen@gmail.com>
2026-03-12 01:25:06 -1000

    Create env.d.ts

---
+++ worker/env.d.ts
@@ -0,0 +1,10 @@
+interface Env {
+  CACHE: KVNamespace;
+  PROJECT_NAME: string;
+  PROJECT_VERSION: string;
+  ENVIRONMENT: string;
+  REGION: string;
+
+  NASA_FIRMS_ENDPOINT: string;
+  NWS_ALERTS_ENDPOINT: string;
+}
